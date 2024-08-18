12a89d66d09d60aa287646152c65f9df85e36627
Andres Henriquez <rymbell@gmail.com>
2024-08-18 22:05:27 +0900

    created db configuration and initialized database in the main file

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,25 @@ import (
 	"log"
 
 	"github.com/ahenla/go-blog/cmd/api"
+	"github.com/ahenla/go-blog/config"
+	"github.com/ahenla/go-blog/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8000", nil)
+	db, err := db.NewSQLDB(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAdress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	server := api.NewAPIServer(":8000", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
---
+++ config/env.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	PublicHost string
+	Port       string
+	DBUser     string
+	DBPassword string
+	DBAdress   string
+	DBName     string
+}
+
+var Envs = initConfig()
+
+func initConfig() Config {
+	return Config{
+		PublicHost: getENV("LOCAL_HOST", "http://localhost"),
+		Port:       getENV("PORT", "8000"),
+		DBUser:     getENV("DB_USER", "root"),
+		DBPassword: getENV("DB_PASSWORD", "C0ntr4s3n4-"),
+		DBAdress:   fmt.Sprintf("%s%s", getENV("DB_HOST", "127.0.0.1"), getENV("DB_PORT", "3306")),
+		DBName:     getENV("DB_NAME", "go-blog"),
+	}
+}
+
+func getENV(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
