36e1644f7820ac25e326a4b87250e0fa14a6b67a
Andres Henriquez <rymbell@gmail.com>
2024-09-11 16:06:34 +0900

    created new package to manage migrations

---
+++ cmd/migrate/migrations/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ahenla/go-blog/config"
+	"github.com/ahenla/go-blog/db"
+	mysqlCfg "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+
+	db, err := db.NewSQLDB(mysqlCfg.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAdress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://cmd/migrate/migrations",
+		"mysql",
+		driver,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
